fe2962511b63d8f9b2c0af766bcd480b8bdd3ecd
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-09-15 13:25:40 +0300

    Add header/filter scroll animation

--- src/includes/header/header.js
+++ src/includes/header/header.js
@@ -1,37 +1,73 @@
+const ClassName = {
+	FIXED: '_fixed',
+};
+
 class Header {
 	constructor() {
 		this.init();
 	}
 	init() {
-		this.container = document.querySelector('.header');
+		this.headerElement = document.querySelector('.page__header');
 
-		if (!this.container) {
+		if (!this.headerElement) {
 			return;
 		}
 
 		this.isHeaderHidden = false;
-		this.headerHeight = this.container.offsetHeight;
-		console.log(this.headerHeight);
+		this.scrollPos = 0;
+		this.filtersElement = document.querySelector('.page__filters');
+		this.filtersElementPos = this.filtersElement.getBoundingClientRect().top;
 
 		this.onWindowScroll = this.onWindowScroll.bind(this);
+		this.onWindowResize = this.onWindowResize.bind(this);
 
 		window.addEventListener('scroll', this.onWindowScroll);
+		window.addEventListener('resize', this.onWindowResize);
 	}
 	onWindowScroll() {
 		this.scrollY = window.scrollY;
-		if (this.scrollY > this.headerHeight && !this.isHeaderHidden) {
-			this.isHeaderHidden = !this.isHeaderHidden;
-			gsap.to(this.container, {
-				yPercent: -100,
-				duration: 0.3,
-			});
-		} else if (this.scrollY < this.headerHeight && this.isHeaderHidden) {
-			this.isHeaderHidden = !this.isHeaderHidden;
-			gsap.to(this.container, {
-				yPercent: 0,
-				duration: 0.1,
-			});
+
+		if (this.scrollY > this.filtersElementPos) {
+			this.filtersElement.classList.add(ClassName.FIXED);
+		} else {
+			this.filtersElement.classList.remove(ClassName.FIXED);
 		}
+
+		if (document.body.getBoundingClientRect().top > this.scrollPos) {
+			// scroll UP
+
+			this.showHeader();
+		} else {
+			// scroll DOWN
+
+			this.hideHeader();
+		}
+		this.scrollPos = document.body.getBoundingClientRect().top;
+	}
+	onWindowResize() {
+		this.scrollY = window.scrollY;
+		this.filtersElementPos = this.filtersElement.getBoundingClientRect().top;
+	}
+	showHeader() {
+		if (this.scrollY !== 0) {
+			this.headerElement.classList.add(ClassName.FIXED);
+		} else {
+			this.headerElement.classList.remove(ClassName.FIXED);
+		}
+		this.isHeaderHidden = !this.isHeaderHidden;
+		gsap.to(this.headerElement, {
+			yPercent: 0,
+			duration: 0.1,
+			ease: 'linear',
+		});
+	}
+	hideHeader() {
+		this.isHeaderHidden = !this.isHeaderHidden;
+		gsap.to(this.headerElement, {
+			yPercent: -100,
+			duration: 0.1,
+			ease: 'linear',
+		});
 	}
 }
 
