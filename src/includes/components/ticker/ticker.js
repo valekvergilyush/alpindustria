58c8792a5b8837903df06a6ecbd1ac1228aa0016
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-09-20 04:28:05 +0300

    Add ticker component

---
+++ src/includes/components/ticker/ticker.js
@@ -0,0 +1,16 @@
+class Ticker {
+	constructor() {
+		this.init();
+	}
+
+	init() {
+		const container = document.querySelector('.ticker__container');
+		const text = container.querySelector('.ticker__text');
+
+		container.append(text.cloneNode(true));
+		container.append(text.cloneNode(true));
+		container.append(text.cloneNode(true));
+	}
+}
+
+export default new Ticker();
