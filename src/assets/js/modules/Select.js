dc32997c42d155a62005180d6f22794bf99d0b72
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-09-30 08:28:15 +0300

    Add rounded select and color select

--- src/assets/js/modules/Select.js
+++ src/assets/js/modules/Select.js
@@ -2,18 +2,54 @@ import customSelect from 'custom-select';
 import Availability from './Availability';
 class Select {
 	constructor() {
-		this.selectBlock = document.querySelector('[data-select]');
+		this.selects = document.querySelectorAll('[data-select]');
 		this.init();
 	}
 
 	init() {
-		this.selects = customSelect(this.selectBlock);
+		if (!this.selects.length) {
+			return;
+		}
+
+		this.selectList = [];
+
 		this.selects.forEach(select => {
-			if (select.select.dataset.select === 'availability') {
-				select.select.addEventListener('change', e => {
+			customSelect(select);
+
+			select.customSelect.container.addEventListener('custom-select:open', () => {
+				select.customSelect.panel.scrollTo(0, 0);
+			});
+
+			if (select.getAttribute('data-select') === 'color') {
+				this._initColorSelect(select);
+			}
+
+			if (select.getAttribute('data-select') === 'availability') {
+				select.addEventListener('change', e => {
 					Availability.checkAvailabilityCity(e.target.value);
 				});
 			}
+		});
+	}
+	_initColorSelect(select) {
+		const openerSpan = select.customSelect.opener.querySelector('span');
+		const options = select.customSelect.select.options;
+		const customOptions = select.customSelect.panel.querySelectorAll('[role="option"]');
+
+		openerSpan.classList.add('custom-select-opener-color');
+		openerSpan.style.backgroundColor = select.value;
+
+		select.addEventListener('change', evt => {
+			openerSpan.style.backgroundColor = evt.target.value;
+		});
+
+		Array.from(options).forEach((opt, index) => {
+			const colorElement = document.createElement('span');
+
+			colorElement.classList.add('custom-select-color');
+			colorElement.style.backgroundColor = opt.value;
+
+			customOptions[index].append(colorElement);
 		});
 	}
 }
