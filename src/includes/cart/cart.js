efae26bfcae943d893f3475854d37fc6bab35609
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-09-30 19:18:08 +0300

    Add back button function

--- src/includes/cart/cart.js
+++ src/includes/cart/cart.js
@@ -24,13 +24,21 @@ class Cart {
 		this.backButton = this.container.querySelector('[data-cart-back]');
 
 		this.onSubmitButtonClick = this.onSubmitButtonClick.bind(this);
+		this.onBackButtonClick = this.onBackButtonClick.bind(this);
 
 		this.deliveryOpener.addEventListener('click', this.onSubmitButtonClick);
+		this.backButton.addEventListener('click', this.onBackButtonClick);
 	}
 	onSubmitButtonClick(evt) {
 		evt.preventDefault();
 
 		this.openDelivery();
+	}
+	onBackButtonClick(evt) {
+		console.log(123);
+		evt.preventDefault();
+
+		this.back();
 	}
 	openDelivery() {
 		gsap.to([this.cartProductsContainer, this.popupCloseButton], {
@@ -51,6 +59,27 @@ class Cart {
 					opacity: 1,
 					display: 'inline-flex',
 					duration: 0.3,
+				});
+			},
+		});
+	}
+	back() {
+		this.container.classList.remove(ClassName.OPENED_DELIVERY);
+		this.cartProductsContainer.classList.remove(ClassName.HIDDEN);
+		this.cartListSection.classList.add(ClassName.HIDDEN);
+		this.deliverySection.classList.add(ClassName.HIDDEN);
+		gsap.to(this.backButton, {
+			opacity: 0,
+			display: 'none',
+			duration: 0.3,
+		});
+		gsap.to(this.popupWrapper, {
+			width: '50%',
+			duration: 0.3,
+			onComplete: () => {
+				gsap.to([this.cartProductsContainer, this.popupCloseButton], {
+					opacity: 1,
+					duration: 0.15,
 				});
 			},
 		});
