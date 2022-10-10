ff57dca24a574cbb59e957115246d9bb1ae1f3ad
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-10-10 19:35:58 +0300

    Add isElementInViewport(el) util

--- src/assets/js/utils/utils.js
+++ src/assets/js/utils/utils.js
@@ -103,6 +103,18 @@ const utils = {
 			document.body.scrollTop
 		);
 	},
+	isElementInViewport(el) {
+		const rect = el.getBoundingClientRect();
+
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <=
+				(window.innerHeight || document.documentElement.clientHeight) /* or $(window).height() */ &&
+			rect.right <=
+				(window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+		);
+	},
 };
 
 export default utils;
