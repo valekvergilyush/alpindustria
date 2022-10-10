eeb2d20ed86e10071ba104383795174779480da5
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-10-10 13:31:32 +0300

    Add cart pay stage

--- src/includes/cart/cart.js
+++ src/includes/cart/cart.js
@@ -1,5 +1,6 @@
 const ClassName = {
 	OPENED_DELIVERY: '_opened-delivery',
+	OPENED_PAY: '_opened-pay',
 	HIDDEN: 'hidden',
 	ANIMATION: '_animation',
 };
@@ -21,11 +22,14 @@ class Cart {
 		this.popupWrapper = document.querySelector('[data-popup-wrapper=cart]');
 		this.deliveryOpener = this.container.querySelector('[data-cart-delivery-opener]');
 		this.backButtons = this.container.querySelectorAll('[data-cart-back]');
+		this.payButton = this.container.querySelector('[data-cart-pay]');
 
 		this.onSubmitButtonClick = this.onSubmitButtonClick.bind(this);
 		this.onBackButtonClick = this.onBackButtonClick.bind(this);
+		this.onPayButtonClick = this.onPayButtonClick.bind(this);
 
 		this.deliveryOpener.addEventListener('click', this.onSubmitButtonClick);
+		this.payButton.addEventListener('click', this.onPayButtonClick);
 		this.backButtons.forEach(button => button.addEventListener('click', this.onBackButtonClick));
 	}
 	onSubmitButtonClick(evt) {
@@ -36,34 +40,59 @@ class Cart {
 	onBackButtonClick(evt) {
 		evt.preventDefault();
 
-		this.openDefault();
+		if (
+			evt.target.getAttribute('data-cart-back') === 'delivery' &&
+			this.popupWrapper.classList.contains(ClassName.OPENED_PAY)
+		) {
+			this.openDelivery();
+		} else {
+			this.openDefault();
+		}
+	}
+	onPayButtonClick(evt) {
+		evt.preventDefault();
+
+		this.openPay();
 	}
 	openDelivery() {
 		this.popupWrapper.classList.add(ClassName.ANIMATION);
-		gsap.to(this.popupWrapper, {
-			width: '100%',
-			duration: 0.3,
-			onComplete: () => {
-				this.popupWrapper.classList.remove(ClassName.ANIMATION);
-				this.popupWrapper.classList.add(ClassName.OPENED_DELIVERY);
-			},
-		});
+		if (this.popupWrapper.classList.contains(ClassName.OPENED_PAY)) {
+			this.popupWrapper.classList.remove(ClassName.OPENED_PAY);
+			this.popupWrapper.classList.remove(ClassName.ANIMATION);
+		} else {
+			this.popupWrapper.classList.add(ClassName.OPENED_DELIVERY);
+			gsap.to(this.popupWrapper, {
+				width: '100%',
+				duration: 0.3,
+				onComplete: () => {
+					this.popupWrapper.classList.remove(ClassName.ANIMATION);
+				},
+			});
+		}
 	}
 	openDefault() {
 		this.popupWrapper.classList.add(ClassName.ANIMATION);
+
 		if (window.innerWidth > TABLET_BREAKPOINT) {
 			gsap.to(this.popupWrapper, {
 				width: '50%',
 				duration: 0.3,
 				onComplete: () => {
 					this.popupWrapper.classList.remove(ClassName.OPENED_DELIVERY);
+					this.popupWrapper.classList.remove(ClassName.OPENED_PAY);
 					this.popupWrapper.classList.remove(ClassName.ANIMATION);
 				},
 			});
 		} else {
 			this.popupWrapper.classList.remove(ClassName.OPENED_DELIVERY);
+			this.popupWrapper.classList.remove(ClassName.OPENED_PAY);
 			this.popupWrapper.classList.remove(ClassName.ANIMATION);
 		}
+	}
+	openPay() {
+		this.popupWrapper.classList.add(ClassName.ANIMATION);
+		this.popupWrapper.classList.add(ClassName.OPENED_PAY);
+		this.popupWrapper.classList.remove(ClassName.ANIMATION);
 	}
 }
 
