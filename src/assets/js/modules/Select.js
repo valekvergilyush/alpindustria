0a1c5466cfff63b350af0f3a07cecfc67adf206f
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-10-10 19:40:24 +0300

    Fix select

--- src/assets/js/modules/Select.js
+++ src/assets/js/modules/Select.js
@@ -1,5 +1,11 @@
 import customSelect from 'custom-select';
 import Availability from './Availability';
+import Utils from '../utils/utils';
+
+const ClassName = {
+	OPENED: '_opened',
+};
+
 class Select {
 	constructor() {
 		this.selects = document.querySelectorAll('[data-select]');
@@ -15,12 +21,18 @@ class Select {
 
 		this.selects.forEach(select => {
 			select.searchField = select.parentElement.querySelector('[data-select-search]');
+			const isColorSelect = select.getAttribute('data-select') === 'color';
+
 			customSelect(select);
 
-			select.customSelect.container.addEventListener('custom-select:open', () => {
-				select.customSelect.panel.scrollTo(0, 0);
-			});
+			select.customSelect.container.addEventListener('custom-select:open', () =>
+				this._onSelectOpen(select)
+			);
+			select.customSelect.container.addEventListener('custom-select:close', () =>
+				this._onSelectClose(select)
+			);
 
+			isColorSelect && this._initColorSelect(select);
 			select.searchField && this._initSearchField(select);
 
 			if (select.getAttribute('data-select') === 'availability') {
@@ -83,6 +95,27 @@ class Select {
 				option.style.display = 'none';
 			}
 		});
+	}
+	_onSelectOpen(select) {
+		const isInViewport = Utils.isElementInViewport(select.customSelect.panel);
+		if (!isInViewport) {
+			select.customSelect.panel.style.transition = 'none';
+			select.customSelect.panel.style.transform = 'translate(0%, -100%)';
+			select.customSelect.panel.style.top = '-0.25rem';
+		}
+
+		select.customSelect.container.parentElement.classList.add(ClassName.OPENED);
+		select.customSelect.panel.scrollTo(0, 0);
+	}
+	_onSelectClose(select) {
+		const isInViewport = Utils.isElementInViewport(select.customSelect.panel);
+		if (!isInViewport) {
+			select.customSelect.panel.style.transform = 'translateY(-5px)';
+			select.customSelect.panel.style.top = '0.25rem';
+			select.customSelect.panel.style.transition =
+				'transition: opacity .15s ease-out,transform .15s ease-out;';
+		}
+		select.customSelect.container.parentElement.classList.remove(ClassName.OPENED);
 	}
 }
 
