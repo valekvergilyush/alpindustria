505c63d5f989d72a4b42c4185a586650dc298453
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-10-12 15:34:09 +0300

    Fix cart select on scroll

--- src/assets/js/modules/Select.js
+++ src/assets/js/modules/Select.js
@@ -107,6 +107,12 @@ class Select {
 
 		select.customSelect.container.parentElement.classList.add(ClassName.OPENED);
 		select.customSelect.panel.scrollTo(0, 0);
+		select.onScroll = () => {
+			select.customSelect.open = false;
+		};
+		setTimeout(() => {
+			document.addEventListener('scroll', select.onScroll, true);
+		}, 300);
 	}
 	_onSelectClose(select) {
 		const isInViewport = Utils.isElementInViewport(select.customSelect.panel);
@@ -117,6 +123,7 @@ class Select {
 				'transition: opacity .15s ease-out,transform .15s ease-out;';
 		}
 		select.customSelect.container.parentElement.classList.remove(ClassName.OPENED);
+		document.removeEventListener('scroll', select.onScroll, true);
 	}
 }
 
