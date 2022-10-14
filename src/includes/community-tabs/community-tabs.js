377c6a1b700b41bd45f5370c0c504bde6b30a4c9
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-10-14 18:20:16 +0300

    Add community tabs section

---
+++ src/includes/community-tabs/community-tabs.js
@@ -0,0 +1,45 @@
+const ClassName = {
+	ACTIVE: '_active',
+};
+
+class CommunityTabs {
+	constructor() {
+		this.init();
+	}
+
+	init() {
+		this.container = document.querySelector('.community-tabs');
+
+		if (!this.container) {
+			return;
+		}
+
+		this.buttons = this.container.querySelectorAll('[data-tab-btn]');
+		this.sections = this.container.querySelectorAll('[data-tab-section]');
+		this.activeButton = this.container.querySelector('[data-tab-btn]._active');
+		this.activeSection = this.container.querySelector('[data-tab-section]._active');
+
+		this._onButtonClick = this._onButtonClick.bind(this);
+
+		this.buttons.forEach(button => button.addEventListener('click', this._onButtonClick));
+	}
+	openSection(id) {
+		this.activeSection && this.activeSection.classList.remove(ClassName.ACTIVE);
+
+		this.activeSection = this.container.querySelector(`[data-tab-section="${id}"]`);
+		this.activeSection.classList.add(ClassName.ACTIVE);
+	}
+	_onButtonClick(evt) {
+		evt.preventDefault();
+
+		const id = evt.target.id;
+
+		this.activeButton && this.activeButton.classList.remove(ClassName.ACTIVE);
+		this.activeButton = this.container.querySelector(`#${id}`);
+		this.activeButton.classList.add(ClassName.ACTIVE);
+
+		this.openSection(id);
+	}
+}
+
+export default new CommunityTabs();
