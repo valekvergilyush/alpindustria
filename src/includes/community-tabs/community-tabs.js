35313374a3e0c668c0e0103ad18cc1348903998a
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-10-19 02:01:25 +0300

    Add community tabs adaptive and PP

--- src/includes/community-tabs/community-tabs.js
+++ src/includes/community-tabs/community-tabs.js
@@ -1,6 +1,9 @@
 const ClassName = {
 	ACTIVE: '_active',
+	MOBILE: '_mobile',
 };
+
+const TABLET_BREAKPOINT = 992;
 
 class CommunityTabs {
 	constructor() {
@@ -19,26 +22,73 @@ class CommunityTabs {
 		this.activeButton = this.container.querySelector('[data-tab-btn]._active');
 		this.activeSection = this.container.querySelector('[data-tab-section]._active');
 
+		this.mqTablet = window.matchMedia(`(max-width: ${TABLET_BREAKPOINT}px)`);
+
 		this._onButtonClick = this._onButtonClick.bind(this);
+
+		const onWindowWidthChange = evt => {
+			if (evt.matches) {
+				this._initMobileTabs();
+			} else {
+				this._initDesktopTabs();
+			}
+		};
+
+		this.mqTablet.addEventListener('change', onWindowWidthChange);
+		onWindowWidthChange(this.mqTablet);
 
 		this.buttons.forEach(button => button.addEventListener('click', this._onButtonClick));
 	}
 	openSection(id) {
-		this.activeSection && this.activeSection.classList.remove(ClassName.ACTIVE);
+		this.activeSection && this._closeActiveSection();
 
+		if (this.activeButton) {
+			this.activeButton.classList.remove(ClassName.ACTIVE);
+			this.activeButton.parentElement.classList.remove(ClassName.ACTIVE);
+		}
+
+		this.activeButton = this.container.querySelector(`#${id}`);
+		this.activeButton.classList.add(ClassName.ACTIVE);
+		this.activeButton.parentElement.classList.add(ClassName.ACTIVE);
+
+		this.container.classList.add(ClassName.ACTIVE);
 		this.activeSection = this.container.querySelector(`[data-tab-section="${id}"]`);
 		this.activeSection.classList.add(ClassName.ACTIVE);
+	}
+	_closeActiveSection() {
+		this.container.classList.remove(ClassName.ACTIVE);
+		this.activeSection.classList.remove(ClassName.ACTIVE);
+
+		this.activeButton.classList.remove(ClassName.ACTIVE);
+		this.activeButton.parentElement.classList.remove(ClassName.ACTIVE);
 	}
 	_onButtonClick(evt) {
 		evt.preventDefault();
 
 		const id = evt.target.id;
 
-		this.activeButton && this.activeButton.classList.remove(ClassName.ACTIVE);
-		this.activeButton = this.container.querySelector(`#${id}`);
-		this.activeButton.classList.add(ClassName.ACTIVE);
+		if (window.innerWidth < TABLET_BREAKPOINT) {
+			if (this.container.classList.contains(ClassName.ACTIVE)) {
+				this._closeActiveSection();
+			} else {
+				this.openSection(id);
+			}
+		} else {
+			this.openSection(id);
+		}
+	}
+	_initMobileTabs() {
+		this.container.classList.add(ClassName.MOBILE);
+		this._closeActiveSection();
+	}
+	_initDesktopTabs() {
+		this.container.classList.remove(ClassName.MOBILE);
 
-		this.openSection(id);
+		if (this.activeButton) {
+			this.openSection(this.activeButton.id);
+		} else {
+			this.openSection(this.buttons[0].id);
+		}
 	}
 }
 
