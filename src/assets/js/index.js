eb70fae4bff99f7694786e6f8f680181b99e7780
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-10-25 18:37:24 +0300

    Add trail video slider

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -39,6 +39,7 @@ class AlpIndustry {
 			CommunitySlider: require('../../includes/community-slider/community-slider').default,
 			CommunityAdvantages: require('../../includes/community-advantages/community-advantages')
 				.default,
+			TrailVideo: require('../../includes/trail-video/trail-video').default,
 		};
 		this.helpers = {
 			ScrollHelper: require('./helpers/ScrollHelper'),
