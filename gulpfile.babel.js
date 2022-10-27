38066c8ae5fe82e563fc8e3dd6fe06860f3f2fae
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-10-27 19:31:57 +0300

    Add json gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,12 +6,13 @@ import fonts from './gulp-tasks/fonts';
 import assetsVersion from './gulp-tasks/assets-version';
 import images from './gulp-tasks/images';
 import svg from './gulp-tasks/svg';
+import jsons from './gulp-tasks/jsons';
 import webpack from './gulp-tasks/webpack';
 import zip from './gulp-tasks/zip';
 import watch from './gulp-tasks/watch';
 import server from './gulp-tasks/server';
 
-gulp.task('build', gulp.parallel(html, styles, fonts, images, svg, webpack));
+gulp.task('build', gulp.parallel(html, styles, fonts, images, svg, jsons, webpack));
 
 gulp.task('production', gulp.series(clean, 'build', assetsVersion));
 
