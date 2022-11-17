06f6a10ed977888d40d236a2d42fabc02222b9b0
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-11-17 20:43:40 +0300

    Add team list animation

--- src/includes/team/team.js
+++ src/includes/team/team.js
@@ -1,8 +1,16 @@
 const ClassName = {
 	ACTIVE: '_active',
+	NEIGHBOR: '_neighbor',
+	OPENING: '_opening',
+	CLOSING: '_closing',
+	TYPE_A: '_a',
+	TYPE_B: '_b',
+	PREV: '_prev',
+	NEXT: '_next',
 };
 
 const MOBILE_BREAKPOINT = 640;
+const TABLET_BREAKPOINT = 992;
 
 class Team {
 	constructor() {
@@ -16,7 +24,7 @@ class Team {
 			return;
 		}
 
-		this.prevWindowWidth = window.innerWidth;
+		this.items = this.container.querySelectorAll('[data-team-item]');
 
 		this.onListClick = this.onListClick.bind(this);
 		this.onWindowResize = this.onWindowResize.bind(this);
@@ -32,33 +40,149 @@ class Team {
 		if (listItem) {
 			evt.preventDefault();
 
-			if (listItem.classList.contains(ClassName.ACTIVE)) {
-				this.closeItem(listItem);
-			} else {
-				this.openItem(listItem);
-			}
+			const isActive = listItem.classList.contains(ClassName.ACTIVE);
+
+			isActive ? this.closeItem(listItem) : this.openItem(listItem);
 		}
 	}
 	openItem(item) {
 		if (window.innerWidth > MOBILE_BREAKPOINT) {
-			this.activeItem && this.activeItem.classList.remove(ClassName.ACTIVE);
+			if (this.activeItem) {
+				this.closeItem(this.activeItem);
+
+				clearTimeout(this.TO2);
+				this.TO2 = setTimeout(() => {
+					this.openItem(item);
+				}, 350);
+				return;
+			}
+		}
+
+		if (this.isAnimating) {
+			return;
 		}
 		this.activeItem = item;
+
+		if (window.innerWidth > MOBILE_BREAKPOINT) {
+			this.neighborElement = this.getNeighborElement(item);
+		}
+
 		this.activeItem.classList.add(ClassName.ACTIVE);
+
+		if (window.innerWidth > MOBILE_BREAKPOINT) {
+			this.addNeighborClass();
+
+			this.isAnimating = true;
+			item.classList.add(ClassName.OPENING);
+			clearTimeout(this.TO);
+			this.TO = setTimeout(() => {
+				this.isAnimating = false;
+				item.classList.remove(ClassName.OPENING);
+			}, 300);
+		}
 	}
 	closeItem(item) {
+		if (this.isAnimating) {
+			return;
+		}
+
 		item.classList.remove(ClassName.ACTIVE);
+		this.removeNeighborClass();
 		this.activeItem = null;
+
+		if (window.innerWidth > MOBILE_BREAKPOINT) {
+			this.isAnimating = true;
+			item.classList.add(ClassName.CLOSING);
+			clearTimeout(this.TO);
+			this.TO = setTimeout(() => {
+				this.isAnimating = false;
+				item.classList.remove(ClassName.CLOSING);
+			}, 300);
+		}
+	}
+	getNeighborElement(item) {
+		const obj = { prev: null, prev2: null, next: null, next2: null };
+
+		obj.prev = item.previousElementSibling;
+		if (obj.prev) {
+			obj.prev2 = obj.prev.previousElementSibling;
+		}
+		obj.next = item.nextElementSibling;
+		if (obj.next) {
+			obj.next2 = obj.next.nextElementSibling;
+		}
+
+		return obj;
+	}
+	addNeighborClass() {
+		this.items.forEach(item => {
+			item.classList.remove(ClassName.TYPE_A);
+			item.classList.remove(ClassName.TYPE_B);
+			item.classList.remove(ClassName.PREV);
+		});
+
+		for (const [pos, element] of Object.entries(this.neighborElement)) {
+			if (element) {
+				element && element.classList.add(ClassName.NEIGHBOR);
+				element.classList.remove(ClassName.PREV);
+				(pos === 'prev' || pos === 'prev2') && element.classList.add(ClassName.PREV);
+			}
+		}
+
+		const cardsA = this.container.querySelectorAll(
+			'[data-team-item]:nth-child(4n+1), [data-team-item]:nth-child(4n+2)'
+		);
+		const cardsLaptopA = this.container.querySelectorAll('[data-team-item]:nth-child(3n+1)');
+		const cardsLaptopB = this.container.querySelectorAll('[data-team-item]:nth-child(3n+3)');
+
+		if (window.innerWidth > TABLET_BREAKPOINT) {
+			cardsA.forEach(item => item.classList.add(ClassName.TYPE_A));
+		} else {
+			cardsLaptopA.forEach(item => item.classList.add(ClassName.TYPE_A));
+			cardsLaptopB.forEach(item => item.classList.add(ClassName.TYPE_B));
+		}
+
+		if (window.innerWidth > TABLET_BREAKPOINT) {
+			if (this.activeItem.classList.contains(ClassName.TYPE_A)) {
+				this.neighborElement.prev && this.neighborElement.prev.classList.remove(ClassName.NEIGHBOR);
+				this.neighborElement.prev2 &&
+					this.neighborElement.prev2.classList.remove(ClassName.NEIGHBOR);
+			} else {
+				this.neighborElement.next && this.neighborElement.next.classList.remove(ClassName.NEIGHBOR);
+				this.neighborElement.next2 &&
+					this.neighborElement.next2.classList.remove(ClassName.NEIGHBOR);
+			}
+		} else if (window.innerWidth > MOBILE_BREAKPOINT) {
+			if (this.activeItem.classList.contains(ClassName.TYPE_A)) {
+				this.neighborElement.prev && this.neighborElement.prev.classList.remove(ClassName.NEIGHBOR);
+				this.neighborElement.prev2 &&
+					this.neighborElement.prev2.classList.remove(ClassName.NEIGHBOR);
+			} else if (this.activeItem.classList.contains(ClassName.TYPE_B)) {
+				this.neighborElement.next && this.neighborElement.next.classList.remove(ClassName.NEIGHBOR);
+				this.neighborElement.next2 &&
+					this.neighborElement.next2.classList.remove(ClassName.NEIGHBOR);
+			} else {
+				this.neighborElement.prev2 &&
+					this.neighborElement.prev2.classList.remove(ClassName.NEIGHBOR);
+				this.neighborElement.next2 &&
+					this.neighborElement.next2.classList.remove(ClassName.NEIGHBOR);
+			}
+		}
+	}
+	removeNeighborClass() {
+		if (this.neighborElement) {
+			for (const [, element] of Object.entries(this.neighborElement)) {
+				element && element.classList.remove(ClassName.NEIGHBOR);
+			}
+		}
 	}
 	onWindowResize() {
-		if (window.innerWidth > MOBILE_BREAKPOINT) {
-			const activeItems = this.container.querySelectorAll(`[data-team-item].${ClassName.ACTIVE}`);
+		const activeItems = this.container.querySelectorAll(`[data-team-item].${ClassName.ACTIVE}`);
 
-			activeItems.length > 1 &&
-				activeItems.forEach(item => {
-					this.closeItem(item);
-				});
-		}
+		activeItems.length > 0 &&
+			activeItems.forEach(item => {
+				this.closeItem(item);
+			});
 	}
 }
 
