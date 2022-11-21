f1374bb6034c26b3fbe89050d2493df73d4067e7
ark1700 <ark1700@yandex.ru>
2022-11-21 13:51:07 +0600

    Fix review

--- src/includes/personal-data-form/personal-data-form.js
+++ src/includes/personal-data-form/personal-data-form.js
@@ -3,8 +3,6 @@ const CLASS_ACTIVE = '_active';
 class PersonalDataForm {
 	constructor() {
 		this.forms = document.querySelectorAll('[data-data-form]');
-		this.openBtns = document.querySelectorAll('[data-data-form-open]');
-		this.closeBtns = document.querySelectorAll('[data-data-form-close]');
 		this.init();
 	}
 
@@ -12,18 +10,24 @@ class PersonalDataForm {
 		if (!this.forms.length) {
 			return;
 		}
+		this.openBtns = document.querySelectorAll('[data-data-form-open]');
+		this.closeBtns = document.querySelectorAll('[data-data-form-close]');
 		this.openBtns.forEach(btn => {
 			btn.addEventListener('click', () => {
 				const formName = btn.dataset.dataFormOpen;
-				const forms = document.querySelector(`[data-data-form="${formName}"]`);
-				forms.classList.add(CLASS_ACTIVE);
+				const form = document.querySelector(`[data-data-form="${formName}"]`);
+				if (form) {
+					form.classList.add(CLASS_ACTIVE);
+				}
 			});
 		});
 		this.closeBtns.forEach(btn => {
 			btn.addEventListener('click', () => {
 				const formName = btn.dataset.dataFormClose;
-				const forms = document.querySelector(`[data-data-form="${formName}"]`);
-				forms.classList.remove(CLASS_ACTIVE);
+				const form = document.querySelector(`[data-data-form="${formName}"]`);
+				if (form) {
+					form.classList.remove(CLASS_ACTIVE);
+				}
 			});
 		});
 	}
