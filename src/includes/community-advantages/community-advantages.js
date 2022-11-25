9f7c9dd3c7b6ad76f5932bd2a0b298b3398e630a
Annie <anniehartmann97@gmail.com>
2022-11-25 16:34:04 +0300

    Remove console.log

--- src/includes/community-advantages/community-advantages.js
+++ src/includes/community-advantages/community-advantages.js
@@ -9,7 +9,6 @@ class CommunityAdvantages {
 
 	init() {
 		this.containers = document.querySelectorAll('[data-community-advantages]');
-		console.log(this.containers);
 
 		if (!this.containers.length || window.innerWidth <= TABLET_BREAKPOINT) {
 			return;
