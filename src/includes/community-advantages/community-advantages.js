30a9a63d9bc6f4525f3c1605cb354c5f07cdf919
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-11-25 14:22:17 +0300

    Add destroy desktop script for mobile on resize

--- src/includes/community-advantages/community-advantages.js
+++ src/includes/community-advantages/community-advantages.js
@@ -14,10 +14,22 @@ class CommunityAdvantages {
 			return;
 		}
 
-		this.containers.forEach(container => this._initSection(container));
+		this.mqTablet = window.matchMedia(`(max-width: ${TABLET_BREAKPOINT}px)`);
+
+		const onWindowWidthChange = evt => {
+			if (evt.matches) {
+				this.containers.forEach(container => this._destroySection(container));
+			} else {
+				this.containers.forEach(container => this._initSection(container));
+			}
+		};
+
+		this.mqTablet.addEventListener('change', onWindowWidthChange);
+		onWindowWidthChange(this.mqTablet);
 	}
 	_initSection(container) {
 		const scrollWrapper = container.querySelector('.community-advantages__wrapper');
+		container.scrollWrapperElement = scrollWrapper;
 
 		const getToValue = () => (scrollWrapper.scrollHeight / window.innerHeight) * window.innerHeight;
 		const isTopPos = scrollWrapper.getAttribute('data-community-advantages-wrapper') === 'top';
@@ -29,7 +41,7 @@ class CommunityAdvantages {
 		}
 
 		const initTopWrapperScrollTrigger = () => {
-			ScrollTrigger.create({
+			container.scrollTrigger = ScrollTrigger.create({
 				trigger: container,
 				start: 'top top',
 				end: `+=${scrollWrapper.scrollHeight - window.innerHeight}`,
@@ -37,7 +49,7 @@ class CommunityAdvantages {
 				invalidateOnRefresh: true,
 				scrub: true,
 			});
-			gsap.set(scrollWrapper, {
+			container.scrollWrapper = gsap.set(scrollWrapper, {
 				y: 0,
 				scrollTrigger: {
 					trigger: container,
@@ -55,7 +67,7 @@ class CommunityAdvantages {
 		};
 
 		const defaultWrapperScrollTrigger = () => {
-			ScrollTrigger.create({
+			container.scrollTrigger = ScrollTrigger.create({
 				trigger: container,
 				start: 'top top',
 				end: `+=${getToValue()}`,
@@ -63,7 +75,7 @@ class CommunityAdvantages {
 				invalidateOnRefresh: true,
 				scrub: true,
 			});
-			gsap.set(scrollWrapper, {
+			container.scrollWrapper = gsap.set(scrollWrapper, {
 				y: 0,
 				scrollTrigger: {
 					trigger: container,
@@ -89,7 +101,7 @@ class CommunityAdvantages {
 		if (container.hasAttribute('data-background')) {
 			const bgColor = container.getAttribute('data-background');
 
-			gsap.to('[data-community-advantages]', {
+			container.bgColor = gsap.to('[data-community-advantages]', {
 				scrollTrigger: {
 					trigger: container,
 					start: 'top bottom',
@@ -99,6 +111,22 @@ class CommunityAdvantages {
 				background: bgColor,
 			});
 		}
+	}
+	_destroySection(container) {
+		container.scrollTrigger && container.scrollTrigger.kill();
+
+		if (container.scrollWrapper) {
+			container.scrollWrapper.kill();
+			container.scrollWrapper.scrollTrigger && container.scrollWrapper.scrollTrigger.kill();
+		}
+
+		if (container.bgColor) {
+			container.bgColor.kill();
+			container.bgColor.scrollTrigger && container.bgColor.scrollTrigger.kill();
+		}
+
+		gsap.set(container, { clearProps: true });
+		gsap.set(container.scrollWrapperElement, { clearProps: true });
 	}
 }
 
