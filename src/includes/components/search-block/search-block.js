ad7ddf3397c0e107638ac62ddde9b2dd56685c0b
ark1700 <ark1700@yandex.ru>
2022-11-28 12:52:36 +0600

    Fixes after test

---
+++ src/includes/components/search-block/search-block.js
@@ -0,0 +1,34 @@
+const CLASS_ACTIVE = '_active';
+
+class SearchBlock {
+	constructor() {
+		this.searchBlocks = document.querySelectorAll('[data-search]');
+		this.init();
+	}
+
+	init() {
+		if (!this.searchBlocks.length) {
+			return;
+		}
+		this.searchBlocks.forEach(searchBlock => {
+			console.log('search');
+			console.log(searchBlock);
+			this.initSearchBlock(searchBlock);
+		});
+	}
+
+	initSearchBlock(block) {
+		const openBtn = block.querySelector('[data-search-open]');
+		if (openBtn) {
+			openBtn.addEventListener('click', () => {
+				block.classList.add(CLASS_ACTIVE);
+			});
+		}
+		block.addEventListener('submit', e => {
+			e.preventDefault();
+			block.classList.remove(CLASS_ACTIVE);
+		});
+	}
+}
+
+export default new SearchBlock();
