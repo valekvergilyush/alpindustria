0db02c66ebc5182d5c15e70c986c470ef5f2b8cb
ark1700 <ark1700@yandex.ru>
2022-11-29 15:27:28 +0600

    Fix search input

--- src/includes/components/search-block/search-block.js
+++ src/includes/components/search-block/search-block.js
@@ -3,6 +3,7 @@ const CLASS_ACTIVE = '_active';
 class SearchBlock {
 	constructor() {
 		this.searchBlocks = document.querySelectorAll('[data-search]');
+		this.clickOutHandler = this.clickOutHandler.bind(this);
 		this.init();
 	}
 
@@ -20,12 +21,26 @@ class SearchBlock {
 		if (openBtn) {
 			openBtn.addEventListener('click', () => {
 				block.classList.add(CLASS_ACTIVE);
+				this.activeSearchBlock = block;
+				setTimeout(() => {
+					document.addEventListener('click', this.clickOutHandler);
+				});
 			});
 		}
 		block.addEventListener('submit', e => {
 			e.preventDefault();
 			block.classList.remove(CLASS_ACTIVE);
+			this.activeSearchBlock = false;
+			document.removeEventListener('click', this.clickOutHandler);
 		});
+	}
+
+	clickOutHandler(e) {
+		if (e.target.closest('[data-search]') || !this.activeSearchBlock) {
+			return;
+		}
+		this.activeSearchBlock.classList.remove(CLASS_ACTIVE);
+		document.removeEventListener('click', this.clickOutHandler);
 	}
 }
 
