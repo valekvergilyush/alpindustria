451c9d758ef8af88d66b23e178e22783f2f58395
ark1700 <ark1700@yandex.ru>
2022-11-29 16:01:41 +0600

    Fix datepicker

--- src/includes/components/input-date/input-date.js
+++ src/includes/components/input-date/input-date.js
@@ -1,10 +1,14 @@
 import flatpickr from 'flatpickr';
+import { Russian } from 'flatpickr/dist/l10n/ru';
 
 const OPTIONS = {
 	disableMobile: 'true',
+	dateFormat: 'd/m/y',
+	locale: Russian,
 };
 class InputDate {
 	constructor() {
+		this.flatpickrs = [];
 		this.init();
 	}
 
@@ -16,8 +20,28 @@ class InputDate {
 		}
 
 		this.inputs.forEach(input => {
-			flatpickr(input, OPTIONS);
+			const minDate = input.dataset.minDate || false;
+			const maxDate = input.dataset.maxDate || false;
+			const flatpickrItem = flatpickr(input, {
+				...OPTIONS,
+				minDate,
+				maxDate,
+			});
+			this.flatpickrs.push(flatpickrItem);
 		});
+
+		const closeFlatpickrBlocks = document.querySelectorAll('[data-flatpickr-scroll="close"]');
+		closeFlatpickrBlocks.forEach(block => {
+			block.addEventListener('scroll', () => {
+				console.log('asdasd');
+				this.flatpickrs.forEach(f => {
+					if (f.isOpen) {
+						f.close();
+					}
+				});
+			});
+		});
+
 	}
 }
 
