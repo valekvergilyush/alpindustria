acaa48526d64f48259a4cfcd0b29c0ccd9295acd
ark1700 <ark1700@yandex.ru>
2022-12-07 12:26:28 +0600

    Add slider on product thumbs

--- src/includes/components/product-slider/product-slider.js
+++ src/includes/components/product-slider/product-slider.js
@@ -1,9 +1,17 @@
 import Flickity from 'flickity';
 import 'flickity-fade';
+import { tns } from 'tiny-slider';
+
+const TABLET_BREAKPOINT = 992;
+const THUMB_SLIDES_NUMBER = 4;
+const INIT_CLASS = '_init';
 class ProductSlider {
 	constructor() {
 		this.sliderBlock = document.querySelector('[data-product-slider]');
 		this.sliderBtns = document.querySelectorAll('[data-slider-btn]');
+		this.sliderThumbsBlock = document.querySelector('[data-product-thumbs-slider]');
+		this.sliderThumbsWrap = document.querySelector('[data-product-thumbs-wrapper]');
+
 		this.init();
 	}
 
@@ -12,6 +20,7 @@ class ProductSlider {
 			return;
 		}
 		this.initSlider();
+		this.initSliderThumbs();
 	}
 
 	initSlider() {
@@ -28,6 +37,50 @@ class ProductSlider {
 				this.sliderBtnHandler(btn);
 			});
 		});
+	}
+
+	initSliderThumbs() {
+		if (!this.sliderThumbsBlock) {
+			return;
+		}
+		this.sliderThumbSlides = this.sliderThumbsBlock.querySelectorAll('.product-slider__thumbnail');
+		if (this.sliderThumbSlides.length <= THUMB_SLIDES_NUMBER) {
+			return;
+		}
+		this.sliderThumbsBlock.classList.add(INIT_CLASS);
+		this.updateSliderThumbs = this.updateSliderThumbs.bind(this);
+		this.sliderThumbs = tns({
+			container: '[data-product-thumbs-slider]',
+			items: THUMB_SLIDES_NUMBER,
+			slideBy: 'page',
+			axis: 'vertical',
+			mouseDrag: false,
+			responsive: {
+				992: {
+					axis: 'horizontal',
+					nav: false,
+					mouseDrag: true,
+				},
+			},
+		});
+		this.updateSliderThumbs();
+		window.addEventListener('resize', this.updateSliderThumbs);
+	}
+
+	updateSliderThumbs() {
+		if (window.innerWidth < TABLET_BREAKPOINT) {
+			this.sliderThumbs.destroy();
+			this.sliderThumbsBlock.classList.add(INIT_CLASS);
+			window.removeEventListener('resize', this.updateSliderThumbs);
+		}
+		const slides = this.sliderThumbsBlock.querySelectorAll('.product-slider__thumbnail');
+		const btn = this.sliderThumbsWrap.querySelector('[data-controls="next"]');
+		slides.forEach(slide => {
+			slide.style.height = this.sliderThumbsWrap.offsetHeight / (THUMB_SLIDES_NUMBER + 1) + 'px';
+		});
+		if (btn) {
+			btn.style.height = this.sliderThumbsWrap.offsetHeight / (THUMB_SLIDES_NUMBER + 1) + 'px';
+		}
 	}
 
 	sliderBtnHandler(btn) {
