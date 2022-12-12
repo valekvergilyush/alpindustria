24536a5f7da52d95146c8420cfd63f293317d2a2
Maxim Kalinin <kalinin.m.a@bk.ru>
2022-12-12 15:45:26 +0300

    Change trails accordion hover

--- src/includes/hero-trails/hero-trails.js
+++ src/includes/hero-trails/hero-trails.js
@@ -1,5 +1,6 @@
 const ClassName = {
 	ACTIVE: '_active',
+	PREV: '_prev',
 };
 
 class HeroTrails {
@@ -16,14 +17,18 @@ class HeroTrails {
 
 		this.triggers = this.accordion.querySelectorAll('[data-vaccordion-item]');
 		this.activeTrigger = this.accordion.querySelector('[data-vaccordion-item]._active');
+		this.lastActive = this.accordion.querySelector('[data-vaccordion-item]._active');
 
 		this.triggers.forEach(trigger => {
 			trigger.addEventListener('click', evt => {
 				evt.preventDefault();
 
 				this.activeTrigger.classList.remove(ClassName.ACTIVE);
+				this.lastActive.classList.remove(ClassName.PREV);
 
+				this.lastActive = this.activeTrigger;
 				this.activeTrigger = evt.currentTarget;
+				this.lastActive.classList.add(ClassName.PREV);
 				this.activeTrigger.classList.add(ClassName.ACTIVE);
 			});
 		});
