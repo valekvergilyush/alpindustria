25923b6c7609718317e04fc6a08c6afd5d378457
Maxim Kalinin <kalinin.m.a@bk.ru>
2023-01-27 13:43:48 +0300

    Remove filters popup

--- src/includes/catalog/catalog.js
+++ src/includes/catalog/catalog.js
@@ -109,16 +109,18 @@ class Catalog {
 	onWindowResize() {
 		this.setFiltersFormWidth();
 
-		if (window.innerWidth <= TABLET_BREAKPOINT && this.layout > 3) {
-			this.setLayout(3);
-		} else if (window.innerWidth <= MOBILE_BREAKPOINT && this.layout > 2) {
-			this.setLayout(2);
-		}
+		if (!HTML_CLASSLIST.contains(ClassName.FILTERS_OPENED)) {
+			if (window.innerWidth <= TABLET_BREAKPOINT && this.layout > 3) {
+				this.setLayout(3);
+			} else if (window.innerWidth <= MOBILE_BREAKPOINT && this.layout > 2) {
+				this.setLayout(2);
+			}
 
-		if (this.layout === 1 && window.innerWidth > MOBILE_BREAKPOINT) {
-			this.setLayout(2);
-		} else if (this.layout === 2 && window.innerWidth > TABLET_BREAKPOINT) {
-			this.setLayout(4);
+			if (this.layout === 1 && window.innerWidth > MOBILE_BREAKPOINT) {
+				this.setLayout(2);
+			} else if (this.layout === 2 && window.innerWidth > TABLET_BREAKPOINT) {
+				this.setLayout(4);
+			}
 		}
 	}
 	setFiltersFormWidth() {
