427f7821472165e1f758d79c370556f57c94e4d0
Maksim Kalinin <kalinin.m.a@bk.ru>
2024-03-22 15:10:12 +0300

    Remove tel autocomplete

--- src/assets/js/modules/Input.js
+++ src/assets/js/modules/Input.js
@@ -106,14 +106,6 @@ class Input {
 					field.classList.remove(ClassName.FILLED);
 				}
 			});
-
-			setInterval(() => {
-				if (input.value) {
-					field.classList.add(ClassName.FILLED);
-				} else {
-					field.classList.remove(ClassName.FILLED);
-				}
-			}, 1000);
 		});
 	}
 }
