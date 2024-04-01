74c98379429603d07e8a6f766fb8eb1c832aba5e
Maksim Kalinin <kalinin.m.a@bk.ru>
2024-04-01 19:52:12 +0300

    Update wishlists page

---
+++ src/includes/components/title-block/title-block.js
@@ -0,0 +1,39 @@
+const ClassName = {
+	EDIT: '_edit',
+};
+
+class TitleBlock {
+	constructor() {
+		this.init();
+	}
+
+	init() {
+		this.container = document.querySelector('[data-title-block]');
+
+		if (!this.container) {
+			return;
+		}
+
+		this.titleContainer = this.container.querySelector('.title-block__wrap');
+		this.formContainer = this.container.querySelector('.title-block__form-wrap');
+		this.editButton = this.container.querySelector('.title-block__link._edit');
+		this.formInput = this.formContainer.querySelector('input');
+		this.formClose = this.formContainer.querySelector('[data-wishlist-cancel]');
+
+		this.editButton.addEventListener('click', evt => {
+			evt.preventDefault();
+
+			this.container.classList.add(ClassName.EDIT);
+			this.formInput.focus();
+		});
+
+		this.formClose.addEventListener('click', evt => {
+			evt.preventDefault();
+
+			this.formInput.value = '';
+			this.container.classList.remove(ClassName.EDIT);
+		});
+	}
+}
+
+export default new TitleBlock();
