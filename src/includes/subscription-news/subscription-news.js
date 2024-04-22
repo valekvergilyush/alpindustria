9bb69a7d870ec4b68dc1498be78016d6a10e1eba
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-04-22 13:52:04 +0300

    Popup validate

--- src/includes/subscription-news/subscription-news.js
+++ src/includes/subscription-news/subscription-news.js
@@ -2,10 +2,18 @@ import Popups from '../../assets/js/modules/Popups';
 
 class SubscriptionNews {
 	constructor() {
+		this.form = document.querySelector('.subscription-news__form');
+		this.input = this.form.querySelector('.subscription-news__input');
 		this.init();
 	}
 	init() {
 		Popups.open('subscription-news');
+
+		this.form.addEventListener('submit', evt => {
+			if (!this.input.classList.contains('is-invalid') && this.input.value !== '') {
+				Popups.open('subscription-success');
+			}
+		});
 	}
 }
 
