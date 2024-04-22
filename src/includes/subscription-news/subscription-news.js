7159717d0ee786c2b9434304e599ccdf041fb1c3
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-04-22 18:51:19 +0300

    Add localStorage

--- src/includes/subscription-news/subscription-news.js
+++ src/includes/subscription-news/subscription-news.js
@@ -3,13 +3,18 @@ import Popups from '../../assets/js/modules/Popups';
 class SubscriptionNews {
 	constructor() {
 		this.form = document.querySelector('.subscription-news__form');
+
+		if (!this.form) return;
+
 		this.input = this.form.querySelector('.subscription-news__input');
 		this.init();
 	}
 	init() {
-		Popups.open('subscription-news');
+		if (!localStorage.getItem('subscription-news')) {
+			Popups.open('subscription-news');
+		}
 
-		this.form.addEventListener('submit', evt => {
+		this.form.addEventListener('submit', () => {
 			if (!this.input.classList.contains('is-invalid') && this.input.value !== '') {
 				Popups.open('subscription-success');
 			}
