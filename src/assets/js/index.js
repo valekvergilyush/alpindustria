fc5f74553c8a8ae615ac98ce1de402816f1a8a06
Maksim Kalinin <kalinin.m.a@bk.ru>
2024-07-18 19:02:18 +0300

    Add cookies

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -95,6 +95,7 @@ class AlpIndustry {
 			DiscountCard: require('../../includes/discount-card/discount-card').default,
 			TitleBlock: require('../../includes/components/title-block/title-block').default,
 			SubscriptionNews: require('../../includes/subscription-news/subscription-news').default,
+			Cookies: require('../../includes/cookies/cookies').default,
 		};
 		this.helpers = {
 			ScrollHelper: require('./helpers/ScrollHelper'),
