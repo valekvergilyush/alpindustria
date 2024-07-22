adc6335db8676bd40e77d89c98cec4f68c944f65
Maksim Kalinin <kalinin.m.a@bk.ru>
2024-07-22 20:21:46 +0300

    Update header

--- src/includes/header/header.js
+++ src/includes/header/header.js
@@ -8,6 +8,7 @@ const ClassName = {
 	HEADER_OPENED: '_header-opened',
 	FILTERS_OPENED: '_filters-opened',
 	HIDDEN_NUM: '_hidden-num',
+	HEADER_CLOSING: '_header-closing',
 };
 
 const Direction = {
@@ -114,14 +115,19 @@ class Header {
 		if (this.menu.animating) {
 			return;
 		}
+
 		this.isHeaderHidden = !this.isHeaderHidden;
 		gsap.to(this.headerElement, {
 			yPercent: -100,
 			duration: 0.1,
 			ease: 'linear',
+			onStart: () => {
+				HTML_CLASSLIST.add(ClassName.HEADER_CLOSING);
+			},
 			onComplete: () => {
 				this.headerElement.classList.remove(ClassName.FIXED);
 				HTML_CLASSLIST.remove(ClassName.HEADER_OPENED);
+				HTML_CLASSLIST.remove(ClassName.HEADER_CLOSING);
 			},
 		});
 		this.timeline &&
