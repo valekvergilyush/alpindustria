30992b01f86c624227fb8b26ce99b92a4ebc873b
Maksim Kalinin <kalinin.m.a@bk.ru>
2024-12-12 18:58:29 +0300

    Update input-date

--- src/includes/components/input-date/input-date.js
+++ src/includes/components/input-date/input-date.js
@@ -1,58 +1,39 @@
 import IMask from 'imask';
-// import Picker from 'pickerjs';
+import flatpickr from 'flatpickr';
+import { Russian } from 'flatpickr/dist/l10n/ru';
 
-// const formatDate = (date = new Date()) => {
-// 	const day = String(date.getDate()).padStart(2, '0');
-// 	const month = String(date.getMonth() + 1).padStart(2, '0');
-// 	const year = date.getFullYear();
+const OPTIONS = {
+	disableMobile: 'true',
+	dateFormat: 'd.m.Y',
+	locale: Russian,
+};
 
-// 	return `${day}.${month}.${year}`;
-// };
-// const addDays = (date = new Date(), qty = 1) => {
-// 	const newDate = new Date(date);
-// 	newDate.setDate(newDate.getDate() + qty);
-
-// 	return newDate;
-// };
-
-// const OPTIONS = {
-// 	format: 'DD.MMMM.YYYY',
-// 	text: {
-// 		cancel: 'Закрыть',
-// 		confirm: 'OK',
-// 	},
-// 	months: [
-// 		'Январь',
-// 		'Февраль',
-// 		'Март',
-// 		'Апрель',
-// 		'Май',
-// 		'Июнь',
-// 		'Июль',
-// 		'Август',
-// 		'Сентябрь',
-// 		'Октябрь',
-// 		'Ноябрь',
-// 		'Декабрь',
-// 	],
-// };
 class InputDate {
 	constructor() {
 		this.init();
 	}
 
 	init() {
-		this.inputs = document.querySelectorAll('[data-datepicker]');
-		// this.rangeDateContainers = document.querySelectorAll('[data-range-date]');
+		const datepickerInputs = document.querySelectorAll('[data-datepicker]');
+		const dateInputs = document.querySelectorAll('[data-dateinput]');
 
-		if (!this.inputs.length) {
-			return;
-		}
+		datepickerInputs.forEach(input => {
+			const minDate = input.dataset.minDate || false;
+			const maxDate = input.dataset.maxDate || false;
+			const defaultDate = input.dataset.minDate === 'today' && new Date();
+			input.flatpickrInstance = flatpickr(input, {
+				...OPTIONS,
+				minDate,
+				maxDate,
+				defaultDate,
+			});
+		});
 
-		this.inputs.forEach(input => {
+		dateInputs.forEach(input => {
 			const dateMask = IMask(input, {
 				mask: Date,
 			});
+			input.dateMask = dateMask;
 
 			input.addEventListener('focus', () => {
 				dateMask.updateOptions({ lazy: false });
@@ -60,38 +41,7 @@ class InputDate {
 			input.addEventListener('blur', () => {
 				dateMask.updateOptions({ lazy: true });
 			});
-
-			// input.datepicker = new Picker(input, OPTIONS);
-			// input.datepicker.picker.classList.add('_date');
-
-			// input.addEventListener('change', () => {
-			// 	input.value = formatDate(input.datepicker.date);
-			// });
 		});
-		/*
-		this.rangeDateContainers.forEach(container => {
-			const startInput = container.querySelector('[data-start-date]');
-			const endInput = container.querySelector('[data-end-date]');
-
-			const start = new Date();
-			const end = addDays(start, 1);
-
-			startInput.value = formatDate(start);
-			endInput.value = formatDate(end);
-
-			[startInput, endInput].forEach(input => {
-				input.addEventListener('change', () => {
-					const startVal = startInput.datepicker.date;
-					const endVal = endInput.datepicker.date;
-
-					if (startVal > endVal) {
-						endInput.value = formatDate(startVal);
-						endInput.datepicker.setDate(startVal);
-					}
-				});
-			});
-		});
-		*/
 	}
 }
 
