1a35ad9db9bc4599b74f8d730344b3f11fe9db0c
Maksim Kalinin <kalinin.m.a@bk.ru>
2024-12-12 15:27:26 +0300

    Update input-date

--- src/includes/components/input-date/input-date.js
+++ src/includes/components/input-date/input-date.js
@@ -1,63 +1,74 @@
-import Picker from 'pickerjs';
+import IMask from 'imask';
+// import Picker from 'pickerjs';
 
-const formatDate = (date = new Date()) => {
-	const day = String(date.getDate()).padStart(2, '0');
-	const month = String(date.getMonth() + 1).padStart(2, '0');
-	const year = date.getFullYear();
+// const formatDate = (date = new Date()) => {
+// 	const day = String(date.getDate()).padStart(2, '0');
+// 	const month = String(date.getMonth() + 1).padStart(2, '0');
+// 	const year = date.getFullYear();
 
-	return `${day}.${month}.${year}`;
-};
-const addDays = (date = new Date(), qty = 1) => {
-	const newDate = new Date(date);
-	newDate.setDate(newDate.getDate() + qty);
+// 	return `${day}.${month}.${year}`;
+// };
+// const addDays = (date = new Date(), qty = 1) => {
+// 	const newDate = new Date(date);
+// 	newDate.setDate(newDate.getDate() + qty);
 
-	return newDate;
-};
+// 	return newDate;
+// };
 
-const OPTIONS = {
-	format: 'DD.MMMM.YYYY',
-	text: {
-		cancel: 'Закрыть',
-		confirm: 'OK',
-	},
-	months: [
-		'Январь',
-		'Февраль',
-		'Март',
-		'Апрель',
-		'Май',
-		'Июнь',
-		'Июль',
-		'Август',
-		'Сентябрь',
-		'Октябрь',
-		'Ноябрь',
-		'Декабрь',
-	],
-};
+// const OPTIONS = {
+// 	format: 'DD.MMMM.YYYY',
+// 	text: {
+// 		cancel: 'Закрыть',
+// 		confirm: 'OK',
+// 	},
+// 	months: [
+// 		'Январь',
+// 		'Февраль',
+// 		'Март',
+// 		'Апрель',
+// 		'Май',
+// 		'Июнь',
+// 		'Июль',
+// 		'Август',
+// 		'Сентябрь',
+// 		'Октябрь',
+// 		'Ноябрь',
+// 		'Декабрь',
+// 	],
+// };
 class InputDate {
 	constructor() {
-		this.flatpickrs = [];
 		this.init();
 	}
 
 	init() {
 		this.inputs = document.querySelectorAll('[data-datepicker]');
-		this.rangeDateContainers = document.querySelectorAll('[data-range-date]');
+		// this.rangeDateContainers = document.querySelectorAll('[data-range-date]');
 
 		if (!this.inputs.length) {
 			return;
 		}
 
 		this.inputs.forEach(input => {
-			input.datepicker = new Picker(input, OPTIONS);
-			input.datepicker.picker.classList.add('_date');
-
-			input.addEventListener('change', () => {
-				input.value = formatDate(input.datepicker.date);
+			const dateMask = IMask(input, {
+				mask: Date,
 			});
-		});
 
+			input.addEventListener('focus', () => {
+				dateMask.updateOptions({ lazy: false });
+			});
+			input.addEventListener('blur', () => {
+				dateMask.updateOptions({ lazy: true });
+			});
+
+			// input.datepicker = new Picker(input, OPTIONS);
+			// input.datepicker.picker.classList.add('_date');
+
+			// input.addEventListener('change', () => {
+			// 	input.value = formatDate(input.datepicker.date);
+			// });
+		});
+		/*
 		this.rangeDateContainers.forEach(container => {
 			const startInput = container.querySelector('[data-start-date]');
 			const endInput = container.querySelector('[data-end-date]');
@@ -80,6 +91,7 @@ class InputDate {
 				});
 			});
 		});
+		*/
 	}
 }
 
