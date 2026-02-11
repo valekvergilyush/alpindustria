ef4dbef3165f2011f772d3af99d3b369049556ab
valekvergilyush <v.vergilyush@gmail.com>
2026-02-11 17:10:46 +0100

    fix

--- paths.js
+++ paths.js
@@ -1,4 +1,16 @@
 export default {
+	build: {
+		html: 'public',
+		scripts: 'public/assets/js/',
+		styles: 'public/assets/css/',
+		images: 'public/assets/images/',
+		fonts: 'public/assets/fonts/',
+		sprites: 'public/assets/images/sprites/',
+		svg: 'public/assets/svg/',
+		jsons: 'public/assets/jsons/',
+		robots: 'public/',
+		docs: 'public/docs/',
+	},
 	public: {
 		html: 'public',
 		scripts: 'public/assets/js/',
