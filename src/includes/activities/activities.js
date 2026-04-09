86f446491a7beb42f7f46468c4e8be040cc3178f
valekvergilyush <v.vergilyush@gmail.com>
2026-04-09 16:19:09 +0200

    rent by call -> slider categories

--- src/includes/activities/activities.js
+++ src/includes/activities/activities.js
@@ -6,34 +6,61 @@ class Activities {
 	}
 
 	init() {
-		const container = document.querySelector('.activities__slider');
+		const containers = document.querySelectorAll('.activities__slider');
 
-		if (!container) {
+		if (!containers.length) {
 			return;
 		}
 
-		const buttonPrev = container.querySelector('.swiper-button-prev');
-		const buttonNext = container.querySelector('.swiper-button-next');
-		const paginationEl = container.querySelector('.swiper-pagination');
+		this.sliders = [];
 
-		this.slider = new Swiper(container, {
-			modules: [Navigation, Pagination],
-			slidesPerView: 'auto',
-			centeredSlides: true,
-			loop: false,
-			pagination: {
-				el: paginationEl,
-				type: 'bullets',
-			},
-			navigation: {
-				prevEl: buttonPrev,
-				nextEl: buttonNext,
-			},
-			breakpoints: {
-				769: {
-					centeredSlides: false,
+		containers.forEach((container) => {
+			const buttonPrev = container.querySelector('.swiper-button-prev');
+			const buttonNext = container.querySelector('.swiper-button-next');
+			const paginationEl = container.querySelector('.swiper-pagination');
+
+			let slidesPerView = 'auto';
+			const isRentCategories = container.hasAttribute('data-rent-categories');
+
+			if (isRentCategories) {
+				const slidesCount = container.querySelectorAll('.swiper-slide').length;
+
+				if (slidesCount <= 4) {
+					slidesPerView = slidesCount;
+					container.classList.add('activities__slider--rent-' + slidesCount);
+				} else {
+					container.classList.add('activities__slider--rent-many');
+				}
+			}
+
+			const swiperConfig = {
+				modules: [Navigation, Pagination],
+				slidesPerView: 'auto',
+				centeredSlides: true,
+				loop: false,
+				pagination: {
+					el: paginationEl,
+					type: 'bullets',
 				},
-			},
+				navigation: {
+					prevEl: buttonPrev,
+					nextEl: buttonNext,
+				},
+				breakpoints: {
+					769: {
+						slidesPerView: slidesPerView,
+						centeredSlides: false,
+					},
+				},
+			};
+
+			if (!isRentCategories) {
+				swiperConfig.slidesPerView = slidesPerView;
+			}
+
+			const slider = new Swiper(container, swiperConfig);
+
+			this.sliders.push(slider);
 		});
 	}
 }
